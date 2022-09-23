768a2fe7325d11f8f0050d9d36dc55429647bdbe
Hasnat <webdevhasnat@gmail.com>
2022-09-23 10:07:09 +0600

    hfhfgh

---
+++ files/data.js
@@ -0,0 +1,66 @@
+const popularProductData = [
+  {
+    id: "a",
+    title: "Lays Chips",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 10,
+    img: "../images/p1.png",
+  },
+  {
+    id: "b",
+    title: "Crispy Chips",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 5,
+    img: "../images/p2.jpg",
+  },
+  {
+    id: "c",
+    title: "Potato Chips",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 12,
+    img: "../images/p3.jpg",
+  },
+  {
+    id: "d",
+    title: "Doritos",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 7,
+    img: "../images/p4.png",
+  },
+  {
+    id: "e",
+    title: "Chocolate Biscuit",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 20,
+    img: "../images/p5.png",
+  },
+  {
+    id: "f",
+    title: "Chocolate Biscuit",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 40,
+    img: "../images/p6.png",
+  },
+  {
+    id: "g",
+    title: "Strawberry Pocky",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 19,
+    img: "../images/p11.png",
+  },
+  {
+    id: "h",
+    title: "Mango Juice",
+    text: `Some quick example text to build on the card title and make up the
+        bulk of the card's content.`,
+    price: 50,
+    img: "../images/product-2.jpg",
+  },
+];
--- files/rest.js
+++ files/rest.js
@@ -4,11 +4,15 @@ let miniCartBody = document.querySelector(".mini-cart-body");
 const cartFooter = document.querySelector(".mini-cart-footer");
 const miniCartCalculation = document.querySelector(".mini-cart-calculation h3");
 const popularEl = document.querySelector(".popular-wrapper");
+const tableBody = document.querySelector("tbody");
+const grandTotal = document.querySelector(".grand-total");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 // sticky nav functionality >>>>
 window.addEventListener("scroll", () => {
   const navbar = document.querySelector(".navbar");
-  navbar.classList.toggle("sticky", window.scrollY > 650);
+  navbar.classList.toggle("sticky", window.scrollY > 380);
 });
 
 // navbar functionality >>>>>
@@ -28,7 +32,6 @@ closeCartIcon.addEventListener("click", showCart);
 blur.addEventListener("click", showCart);
 
 function showCart() {
-  console.log(1);
   if (miniCart.classList.contains("show-mini-cart")) {
     miniCart.classList.remove("show-mini-cart");
     blur.style.display = "none";
@@ -38,57 +41,11 @@ function showCart() {
   }
 }
 
-// popular product js start >>>>>
-
-let basket = JSON.parse(localStorage.getItem("data")) || [];
-
-function generatePopularProduct() {
-  popularProductData.forEach((data) => {
-    const { id, title, text, img, price } = data;
-
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <article id=${id} class="popular-product ">
-    <img  src=${img} class="product-img" />
-    <div class="product-body">
-      <h5 class="product-title"> ${title} </h5>
-      <p class="product-text">
-        ${text}
-      </p>
-      <h5 class="product-price">$${price}</h5>
-        <button onclick="addToCart(${id})" href="#" class="popular-btn">Add To Cart </button>
-        </div>
-        </div>
-        </article>
-        `;
-    popularEl.appendChild(div);
-  });
-}
-generatePopularProduct();
-
-function addToCart(id) {
-  const selectedProduct = id.id;
-
-  const searchBasket = basket.find((product) => product.id === selectedProduct);
-
-  if (searchBasket === undefined) {
-    basket.push({
-      id: selectedProduct,
-      quantity: 1,
-    });
-    localStorage.setItem("data", JSON.stringify(basket));
-    updateCart();
-  } else return;
-}
-
 function updateCart() {
   const cartQuantity = document.querySelector(".quantity");
   cartQuantity.innerText = basket.length;
-  generateMiniCart();
 }
 updateCart();
-
-// popular product js end >>>>>
 
 // mini cart js start >>>>>
 
@@ -112,23 +69,23 @@ function generateMiniCart() {
       const minicartDiv = document.createElement("div");
 
       minicartDiv.innerHTML = `
-      <article  class="mini-cart-item">
-            <div class='mini-cart-left'>
-            <img src=${img} alt="" />
-            <p> ${product.quantity} </p>
-            </div>
-            <div class="info">
-              <h4> ${title} </h4>
-              <div class="plus-minus-icon">
-                <i onclick=" decrement(${id})" class="fa-solid fa-minus"></i>
-                <p id = ${id}>$${price * product.quantity} </p>
-                <i onclick=" increment(${id})" class="fa-solid fa-plus"></i>
+        <article  class="mini-cart-item">
+              <div class='mini-cart-left'>
+              <img src=${img} alt="" />
+              <p> ${product.quantity} </p>
               </div>
-            </div>
-            <i onclick="removeMiniCartItem(${id})" class="fa-solid fa-xmark"></i>
-  
-          </article>
-      `;
+              <div class="info">
+                <h4> ${title} </h4>
+                <div class="plus-minus-icon">
+                  <i onclick=" decrement(${id})" class="fa-solid fa-minus"></i>
+                  <p id = ${id}>$${price * product.quantity} </p>
+                  <i onclick=" increment(${id})" class="fa-solid fa-plus"></i>
+                </div>
+              </div>
+              <i onclick="removeMiniCartItem(${id})" class="fa-solid fa-xmark"></i>
+    
+            </article>
+        `;
       miniCartBody.appendChild(minicartDiv);
     });
     miniCartCalculation.innerText = `Total Price : $${totalPrice}`;
@@ -136,32 +93,36 @@ function generateMiniCart() {
     cartFooter.style.display = "block";
   }
 }
+generateMiniCart();
 
 function removeMiniCartItem(id) {
-  const selectedProduct = id[0];
-  basket = basket.filter((item) => selectedProduct.id !== item.id);
+  const selectedProduct = id.id;
+  basket = basket.filter((item) => selectedProduct !== item.id);
   localStorage.setItem("data", JSON.stringify(basket));
   generateMiniCart();
+  generateCartPage();
   updateCart();
 }
 
 function increment(id) {
-  const selectedProduct = id[0];
+  const selectedProduct = id.id;
 
-  const search = basket.find((item) => item.id === selectedProduct.id);
+  const search = basket.find((item) => item.id === selectedProduct);
   search.quantity++;
   localStorage.setItem("data", JSON.stringify(basket));
   generateMiniCart();
+  generateCartPage();
 }
 
 function decrement(id) {
-  const selectedProduct = id[0];
-  const search = basket.find((item) => item.id === selectedProduct.id);
+  const selectedProduct = id.id;
+  const search = basket.find((item) => item.id === selectedProduct);
 
   if (search.quantity > 1) {
     search.quantity--;
     localStorage.setItem("data", JSON.stringify(basket));
     generateMiniCart();
+    generateCartPage();
   }
 }
 
@@ -169,6 +130,53 @@ function clearCart() {
   basket = [];
   localStorage.setItem("data", JSON.stringify(basket));
   generateMiniCart();
+  generateCartPage();
   updateCart();
 }
 // mini cart js end >>>>>
+
+// cart page functionality start >>>>>>
+
+function generateCartPage() {
+  tableBody.innerHTML = "";
+
+  const item = basket.map((product) => {
+    const search = popularProductData.find((data) => data.id === product.id);
+    return product.quantity * search.price;
+  });
+  const totalPrice = item.reduce((cur, sum) => cur + sum, 0);
+
+  basket.forEach((item) => {
+    const search = popularProductData.find((product) => product.id === item.id);
+    const { id, img, price, title } = search;
+
+    const cartTr = document.createElement("tr");
+
+    cartTr.innerHTML = `
+    <td><img src=${img} /></td>
+    <td> ${title} </td>
+    <td>$${price}</td>
+    <td class="qty">
+      <i onclick="decrement(${id})" class="fa-solid fa-minus"></i>
+      <p> ${item.quantity}</p>
+      <i onclick="increment(${id})" class="fa-solid fa-plus"></i>
+    </td>
+    <td>$${item.quantity * price}</td>
+    <td><i onclick='removeMiniCartItem(${id})' class="fa-solid fa-xmark"></i></td>
+    `;
+
+    tableBody.appendChild(cartTr);
+  });
+
+  if (basket.length === 0) {
+    grandTotal.innerHTML = "";
+  } else {
+    grandTotal.innerHTML = `
+    <h2>Total Price : $ ${totalPrice}</h2>
+          <a class="checkout" href="#">Checkout</a>
+    `;
+  }
+}
+generateCartPage();
+
+// cart page functionality end >>>>>>
